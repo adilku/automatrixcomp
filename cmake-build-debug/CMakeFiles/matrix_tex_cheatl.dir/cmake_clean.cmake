f3b31a7aae1426e247ade493d5a8c1f0532e6677
adil <adhan2a@gmail.com>
2021-01-21 15:52:43 +0300

    some changes

---
+++ cmake-build-debug/CMakeFiles/matrix_tex_cheatl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_tex_cheatl.dir/matrix_linalg.cpp.o"
+  "matrix_tex_cheatl.pdb"
+  "matrix_tex_cheatl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_tex_cheatl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
